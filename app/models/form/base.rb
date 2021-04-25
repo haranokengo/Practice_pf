1e8dbb08bfbd7e7b97881237740fcb8316c5001a
haranokengo <haranokengo@gmail.com>
2021-04-25 08:56:39 +0000

    コメント書いた

--- app/models/form/base.rb
+++ app/models/form/base.rb
@@ -1,4 +1,5 @@
 class Form::Base
+  # Formモデルでメソッドを使えるようにしている
   include ActiveModel::Model
   include ActiveModel::Callbacks
   include ActiveModel::Validations
--- app/models/form/valiablecost_value_collection.rb
+++ app/models/form/valiablecost_value_collection.rb
@@ -1,9 +1,13 @@
 class Form::ValiablecostValuesCollection < Form::Base
   FORM_COUNT = 10 #ここで、作成したい登録フォームの数を指定
+  # valiablecost_valuesが他で使われないようにしている。
+  # ここでは、initialize,valiablecost_values_attributes,saveのみ
   attr_accessor :valiablecost_values
 
   def initialize(attributes = {})
+    # データを表示できるようにしている機能ではそこまで必要ない
     super attributes
+    # selfは（自分自身のといういう意味）ここではForm::ValiablecostValuesのこと
     self.valiablecost_values = FORM_COUNT.times.map { ValiablecostValue.new() } unless self.valiablecost_values.present?
   end
 
